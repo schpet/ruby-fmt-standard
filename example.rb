d0be09ae6ce47fff695cc913f99ca170561f0cee
Peter Schilling <peter@peterschilling.org>
2023-02-02 09:10:59 -0800

    Run standardrb --fix

--- example.rb
+++ example.rb
@@ -1,20 +1,19 @@
 module Example
   scope(
     :search,
-    -> (term) { do_stuff(term) }
+    ->(term) { do_stuff(term) }
   )
 
   def foo(scope_name)
     do_stuff
 
     @scope_name = scope_name.to_sym
-
   rescue => e
     puts("uh oh: #{e}")
   end
 
   def bar(conf)
-    if self.conf() == :foo
+    if self.conf == :foo
       yep
     end
   end

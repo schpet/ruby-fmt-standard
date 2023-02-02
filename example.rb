156262742b7c5b749b6e59155d3bb3fb302e2fb2
Peter Schilling <peter@peterschilling.org>
2023-02-02 08:47:49 -0800

    Add some more disagreements

--- example.rb
+++ example.rb
@@ -4,10 +4,18 @@ module Example
     -> (term) { do_stuff(term) }
   )
 
-  def foo
+  def foo(scope_name)
     do_stuff
+
+    @scope_name = scope_name.to_sym
 
   rescue => e
     puts("uh oh: #{e}")
+  end
+
+  def bar(conf)
+    if self.conf() == :foo
+      yep
+    end
   end
 end
